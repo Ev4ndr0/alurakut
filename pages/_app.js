fa7f5d9aef3ccc2eb43ca008f0d7a0c43cbe6dcc
Ev4ndr0 <evandro.macedo5@gmail.com>
2021-07-19 20:34:57 -0300

    atualizado

--- pages/_app.js
+++ pages/_app.js
@@ -8,20 +8,24 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
   body {
     font-family: sans-serif;
     background-color: #D9E6F6;
   }
+
   #__next {
     display: flex;
     min-height: 100vh;
     flex-direction: column;
   }
+
   img {
     max-width: 100%;
     height: auto;
     display: block;
   }
+
   ${AlurakutStyles}
 `
 
